6b39fa32a652fbc318075c7710c918832c7b8ae4
Aditya24 <aadityabante@gmail.com>
2020-09-08 21:19:29 +0200

    Updates

---
+++ precache-manifest.4856c845b805e1aaaca76eb82a63ed87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dada574ec057d4476a8156e31ba49e9",
+    "url": "/project-robofriends-react-js/index.html"
+  },
+  {
+    "revision": "d388a0e4fa8e455142bd",
+    "url": "/project-robofriends-react-js/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8359efe9e0629c2bfee5",
+    "url": "/project-robofriends-react-js/static/css/main.3de8c3df.chunk.css"
+  },
+  {
+    "revision": "d388a0e4fa8e455142bd",
+    "url": "/project-robofriends-react-js/static/js/2.eb53dfda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-robofriends-react-js/static/js/2.eb53dfda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8359efe9e0629c2bfee5",
+    "url": "/project-robofriends-react-js/static/js/main.51860724.chunk.js"
+  },
+  {
+    "revision": "5957b82b0de3260085db",
+    "url": "/project-robofriends-react-js/static/js/runtime-main.b5c5ddff.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/project-robofriends-react-js/static/media/SEGA.12a3db8b.woff"
+  }
+]);
